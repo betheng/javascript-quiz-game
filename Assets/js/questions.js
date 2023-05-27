f7df74c20187f0a8dff575e23a307a13d477da83
betheng <131614496+betheng@users.noreply.github.com>
2023-05-27 17:25:10 -0400

    add questions to JS file which will render to quiz taker

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -1,9 +1,34 @@
 // list of all questions, choices, and answers
-var questions = [
+const questions = [
   {
-    title: 'Test Question',
-    choices: ['placeholder 1', 'placeholder 2', 'placeholder 3', 'placeholder 4'],
-    answer: 'placeholder 1',
+    title: 'As of 2022, where did JavaScript rank for most popular languages in the world ?',
+    choices: ['1', '2', '3', '4'],
+    answer: '3',
   },
-
+  {
+    title: 'What does JavaScript stand for?',
+    choices: ['Javamocha Scripture', 'Javalin Scription', 'JavaScript', 'Java Core Scripting Methodology'],
+    answer: 'JavaScript',
+  },
+  {
+    title: 'What year was JavaScript created?',
+    choices: ['1989', '1990', '1995', '2001'],
+    answer: '1995',
+  },
+  {
+    title: 'What browser was JavaScript originally created for?',
+    choices: ['Netscape 2', 'AOL', 'Internet Explorer', 'Safari'],
+    answer: 'Netscape 2',
+  },
+  {
+    title: 'What type of programming language is JavaScript?',
+    choices: ['Core Processing', 'Object Oriented', 'NaN', 'Lean Java'],
+    answer: 'Object Oriented',
+  },
+  {
+    title: 'In Javascript, what does i++ do?',
+    choices: ['Adds 2 to i', 'Nothing, the 2 plus signs cancel each other out', 'Adds 1 to i', 'Changes variable "i" to a number'],
+    answer: 'Adds 1 to i',
+  },
+  
 ];
